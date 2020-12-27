1b4f322d9f939ed2072ea97bc56a53140165504f
Osama Ahmed <osamaahmed.sus@gmail.com>
2020-12-27 18:58:50 +0200

    Prepupulate users and questions tables using model factories and seed

--- app/Models/Question.php
+++ app/Models/Question.php
@@ -4,14 +4,23 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Question extends Model
 {
     use HasFactory;
-    protected $fillable = ['title', 'body']
+    protected $fillable = ['title', 'body'];
     
     public function user() {
     		return $this->belongsTo(User::class);
+
+    }
+
+     public function setTitleAttribute($value){
+
+        $this->attributes['title'] = $value;
+        $this->attributes['slug'] = str::slug($value);
+        
 
     }
     // $question = Question::find(1);
--- app/Models/User.php
+++ app/Models/User.php
@@ -48,11 +48,5 @@ class User extends Authenticatable
 
 
     }
-    public function setTitleAttribute($value){
-
-        $this->attributes['title'] = $value;
-        $this->attributes['slug'] = str_slug($value);
-        
-
-    }
+   
 }
