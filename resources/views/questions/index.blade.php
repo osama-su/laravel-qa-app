24cecb254cc4a995cf1c0a152d5455442df3483a
Osama Ahmed <osamaahmed.sus@gmail.com>
2020-12-28 01:20:44 +0200

    compiling assets and fixing pagination

--- resources/views/questions/index.blade.php
+++ resources/views/questions/index.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-12">
             <div class="card">
                 <div class="card-header">{{ __('All Questions') }}</div>
 
@@ -15,9 +15,6 @@
                                     {{ $question->title }}
                                 </h3>
                                 
-
-                                
-
                                  {{Illuminate\Support\Str::limit($question->body, 250)}} 
                                  
                             </div>
@@ -25,7 +22,7 @@
                         <hr>
                     @endforeach
 
-                    {{$questions->links()}}
+                    {{ $questions->links('pagination::bootstrap-4')}}
                 </div>
             </div>
         </div>
