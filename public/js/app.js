b348e86373e8e6fcee11a204324be652200cbbf6
Osama Ahmed <osamaahmed.sus@gmail.com>
2021-01-25 18:50:30 +0200

    Implement delete functionality for the question

--- public/js/app.js
+++ public/js/app.js
@@ -49886,8 +49886,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! D:\Workspace\PHPnew\laravel-qa-app\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! D:\Workspace\PHPnew\laravel-qa-app\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\xampp\htdocs\laravel-qa-app\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\xampp\htdocs\laravel-qa-app\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
--- resources/views/questions/index.blade.php
+++ resources/views/questions/index.blade.php
@@ -38,6 +38,11 @@
                                 </h3>
                                 <div class="ml-auto">
                                     <a href="{{ route('questions.edit', $question->id) }}" class="btn btn-sm btn-outline-info">Edit</a>
+                                    <form class="form-delete" method="post" action="{{ route('questions.destroy', $question->id) }}">
+                                        @method('DELETE')
+                                        @csrf
+                                        <button type="submit" class="btn btn-sm btn-outline-danger" onclick="return confirm('Are you sure you want to delete this question ?')"> Delete </button>
+                                    </form>
                                 </div>
                                 </div>
                                 
